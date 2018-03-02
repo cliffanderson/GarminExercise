cd70733b4caff41e771fe7361aa6478e51690df7
Cliff Anderson <cliffordmorgananderson@gmail.com>
2018-03-01 20:11:45 -0500

    Code tested and complete

--- src/GarminExercise.java
+++ src/GarminExercise.java
@@ -12,8 +12,69 @@ import java.util.List;
 
 public class GarminExercise {
 
+    private static Node buildGarminTree() {
+        // Create nodes
+        Node root = new Node("Start");
+        Node A1 = new Node("A1");
+        Node A2 = new Node("A2");
+        Node B1 = new Node("B1");
+        Node B2 = new Node("B2");
+        Node C1 = new Node("C1");
+        Node D1 = new Node("D1");
+        Node E1 = new Node("E1");
+        Node findMe = new Node("Find Me");
+
+        // Build tree
+        root.getChildren().add(A1);
+        root.getChildren().add(A2);
+
+        A1.getChildren().add(D1);
+
+        D1.getChildren().add(E1);
+
+        A2.getChildren().add(B1);
+        A2.getChildren().add(B2);
+
+        B1.getChildren().add(findMe);
+
+        B2.getChildren().add(C1);
+
+        return root;
+    }
+
+    private static Node findNodeByName(Node node, String targetName) {
+        // Logging
+        System.out.printf("findNodeByName()  node=%s  targetName=%s%n", node.getName(), targetName);
+
+        // Did we find the right node?
+        if(node.getName().equals(targetName)) {
+            // Yes!
+            return node;
+        }
+
+        // We are still searching for the right node
+        // For each child node, make a recursive call
+        for(Node n : node.getChildren()) {
+            Node foundNode = findNodeByName(n, targetName);
+
+            if(foundNode != null) {
+                // If it's not null we are successful in finding the node
+                return foundNode;
+            }
+        }
+
+        // Return null because we didn't find the node
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Node root = buildGarminTree();
+        String targetName = "Find Me";
+
+        Node foundNode = findNodeByName(root, targetName);
+
+        System.out.printf("%n%nThe node with name %s %s found", targetName, (foundNode == null ? "was not" : "was"));
+
     }
 }
 
